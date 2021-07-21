8c224c28c0b916a91a713bb79669d4a10fd10990
shrey2199 <52773351+shrey2199@users.noreply.github.com>
2021-07-21 17:24:10 +0530

    removed config file by mistake

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+	LD_DOMAIN = os.environ.get("LD_DOMAIN", "") # Without https:// or http://
+	SECRET = os.environ.get("SECRET", "")
+	ADMIN_IDS = os.environ.get("ADMIN_IDS", "") # ==> Enter Admin Chat IDs here !!
+	PIC = os.environ.get("PIC", "")
