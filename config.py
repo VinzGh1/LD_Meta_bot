f2a0ab56828af3bbfb33fee7e1e45c705bd87fe2
shrey2199 <52773351+shrey2199@users.noreply.github.com>
2021-07-25 18:51:25 +0530

    restart heroku dynos and view dyno stats

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ class Config(object):
 	SECRET = os.environ.get("SECRET", "")
 	ADMIN_IDS = os.environ.get("ADMIN_IDS", "") # ==> Enter Admin Chat IDs here !!
 	PIC = os.environ.get("PIC", "")
+	HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
+	HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "")
+	HEROKU_EMAIL = os.environ.get("HEROKU_EMAIL", "")
